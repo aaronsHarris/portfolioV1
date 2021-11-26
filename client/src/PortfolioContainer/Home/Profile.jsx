606d480002e6a00e453e37f14d897cfdfa2f2b9d
Aaron Harris <aaronsharris@gmail.com>
2021-11-26 14:56:58 -0600

    added divs to profile and install react-typical

--- client/src/PortfolioContainer/Home/Profile.jsx
+++ client/src/PortfolioContainer/Home/Profile.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function Profile() {
-    return (
-        <div className="profile-container">
-            <div className="profile-parent">
-                <div className="profile-details">
-                    <div className="colz">
-                        <a href="#">
-                            <i className='fa fa-Github'></i>
-                        </a>
-                        <a href="#">
-                            <i className='fa fa-Linkedin'></i>
-                        </a>
-                        <a href="#">
-                            <i className='fa fa-Github'></i>
-                        </a>
-                        <a href="#">
-                            <i className='fa fa-Github'></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://github.com/aaronsHarris">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/aaron-harris-577867218/">
+              <i className="fa fa-linkedin"></i>
+            </a>
+
+            <div className="profile-detail-names">
+              <span className="primary-text">
+                {" "}
+                Hello, I'm <span className="highlighted-text">Aaron</span>
+              </span>
+                      </div>
+                      <div className="profile-details-role">
+                          <span className="primary-text">
+                              {" "}
+                              <h1></h1>`
+                          </span>
+                      </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
