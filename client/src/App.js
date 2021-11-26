0e20973aa33e0a67f67007f45b2c4a198fc171ae
Aaron Harris <aaronsharris@gmail.com>
2021-11-26 17:38:14 -0600

    added profile footer and footer styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 import Profile from './PortfolioContainer/Home/Profile.jsx'
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Footer />
     </div>
   );
 }
