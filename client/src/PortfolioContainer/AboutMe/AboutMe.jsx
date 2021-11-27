17727e2a580f3980a53266a7544eb710750f47c0
Aaron Harris <aaronsharris@gmail.com>
2021-11-27 00:46:24 -0600

    rendered about me to the page

--- client/src/PortfolioContainer/AboutMe/AboutMe.jsx
+++ client/src/PortfolioContainer/AboutMe/AboutMe.jsx
@@ -1,15 +1,68 @@
-import { useEffect } from "react"
-import ScreenHeading from '../../utils/ScreenHeading/ScreenHeading'
-import ScrollService from '../../utils/ScrollService'
-import Animations from '../../utils/Animations'
-
+import { useEffect } from "react";
+import ScreenHeading from "../../utils/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utils/ScrollService";
+import Animations from "../../utils/Animations";
 
 export default function AboutMe(props) {
-    return (
-        <div className='about-me-container'>
-            <div className="about-me-parent">
-            <ScreenHeading title={'About me'} subHeading={"Why Choose Me?"} />
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const SCREEN_CONSTANTS = {
+    description:
+      "I am a full stack developer that wants to help create the future of tech. As a Chef and business manager in the hospitality industry I always thrived in the fiery pace of the kitchen or the customer satisfaction of the dining room. I love pushing myself in my personal life with cycling and being creative through writing or comedy. As a software engineer I will consistently bring my fire and passion for the customer and innovative products to my work",
+    highlights: {
+      bullets: [
+        "Full Stack and mobile development",
+        "Interactive Front End Design",
+        "React Development",
+        "Building REST API",
+        "Managing Database",
+        "OOP Development",
+      ],
+      heading: "Here are a few Highlights:",
+    },
+  };
+  const renderHighlights = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="about-me-container" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlights-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlights()}
             </div>
+            <div className="about-me-options">
+              <button className="btn primary-btn"> Hire Me</button>
+              <a
+                href="../../assets/Current resume dec19 (1).pdf"
+                download="../../assets/Current resume dec19 (1).pdf"
+              >
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/src/utils/ScrollService.js
+++ client/src/utils/ScrollService.js
@@ -1,73 +1,71 @@
 import { TOTAL_SCREENS } from "./commonUtils";
 import { Subject } from "rxjs";
-import { object } from 'prop-types'
 
 export default class ScrollService {
-    static scrollHandler = new ScrollService()
+  static scrollHandler = new ScrollService();
 
-    static currentScreenBroadCaster = new Subject()
-    static currentScreenFading = new Subject()
+  static currentScreenBroadCaster = new Subject();
+  static currentScreenFadeIn = new Subject();
 
-    constructor() {
-        window.addEventListener('scroll', this.checkCurrentScreenUnderViewport)
+  constructor() {
+    window.addEventListener("scroll", this.checkCurrentScreenUnderViewport);
+  }
+
+  scrollToHireMe = () => {
+    let contactMeScreen = document.getElementById("Contact Me");
+    if (!contactMeScreen) return;
+    contactMeScreen.scrollIntoView({ behavior: "smooth" });
+  };
+  scrollToHome = () => {
+    let homeScreen = document.getElementById("Home");
+    if (!homeScreen) return;
+    homeScreen.scrollIntoView({ behavior: "smooth" });
+  };
+
+  isElementInView = (elem, type) => {
+    let rec = elem.getBoundingClientRect();
+    let elementTop = rec.top;
+    let elementBottom = rec.bottom;
+
+    let partiallyVisable =
+      elementTop < window.innerHeight && elementBottom >= 0;
+    let completlyVisible =
+      elementTop >= 0 && elementBottom <= window.innerHeight;
+
+    switch (type) {
+      case "partial":
+        return partiallyVisable;
+
+      case "complete":
+        return completlyVisible;
+      default:
+        return false;
     }
+  };
 
-    scrollToHireMe = () => {
-        let contactMeScreen = document.getElementById("Contact Me")
-        if (!contactMeScreen) return
-        contactMeScreen.scrollIntoView({behavior: "smooth"})
-    }
-    scrollToHome = () => {
-        let homeScreen = document.getElementById("Home")
-        if (!homeScreen) return
-        homeScreen.scrollIntoView({behavior: "smooth"})
-    }
+  checkCurrentScreenUnderViewport = (e) => {
+    if (!e || Object.keys(e).length < 1) return;
+    for (let screen of TOTAL_SCREENS) {
+      let screenFromDom = document.getElementById(screen.screen_name);
+      if (!screenFromDom) continue;
 
-    isElementInView = (elem, type) => {
-        let rec = elem.getboundingClientRect()
-        let elementTop = rec.top
-        let elementBottom = rec.bottom
-
-        let partiallyVisable = elementTop < window.innerHeight && elementBottom >= 0
-        let completlyVisible = elementTop >= 0 && elementBottom <= window.innerHeight
-
-        switch (type) {
-            case 'partial':
-                return partiallyVisable;
-            
-            case 'complete':
-                return completlyVisible;
-            default:
-                return false
+      let fullyVisible = this.isElementInView(screenFromDom, "complete");
+      let partiallyVisible = this.isElementInView(screenFromDom, "partial");
+      if (fullyVisible || partiallyVisible) {
+        if (partiallyVisible && !screen.alreadyRendered) {
+          ScrollService.currentScreenFadeIn.next({
+            fadeInScreen: screen.screen_name,
+          });
+          screen["alreadyRendered"] = true;
+          break;
         }
-    }
-
-    checkCurrentScreenUnderViewport = (e) => {
-        if (!e || Object.keys(e).length < 1)
-            return;
-        for (let screen of TOTAL_SCREENS) {
-            let screenFromDom = document.getElementById(screen.screen_name);
-            if (!screenFromDom)
-                continue;
-            
-            let fullyVisible = this.isElementInView(screenFromDom, "complete")
-            let partiallyVisible = this.isElementInView(screenFromDom, "partial")
-
-            if (fullyVisible || partiallyVisible) {
-                if (partiallyVisible && !screen.alreadyRendered) {
-                    ScrollService.currentScreenFadeIn.next({
-                        fadeInScreen: screen.screen_name
-                    })
-                    screen['alreadyRendered'] = true
-                    break;
-                }
-                if (fullyVisible) {
-                    ScrollService.currentScreenBroadCaster.next({
-                        screenInview: screen.screen_name
-                    });
-                    break;
-                }
-            }
+        if (fullyVisible) {
+          ScrollService.currentScreenBroadCaster.next({
+            screenInview: screen.screen_name,
+          });
+          break;
         }
+      }
     }
+  };
 }
