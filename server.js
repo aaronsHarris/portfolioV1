8a162aa053d08403a648ae35b8e4f98d4ab51cc1
Aaron Harris <aaronsharris@gmail.com>
2021-11-29 23:27:06 -0600

    deploy to heroku

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+require("dotenv").config;
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-require("dotenv").config
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+const contactRoute = require("./route/contactRoute.js");
 
-const contactRoute = require('./route/contactRoute.js')
-
-
-const app = express()
+const app = express();
 
 //MIDDLEWARE
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/", contactRoute)
+app.use("/", contactRoute);
 
-const port = process.env.PORT || 3000
-app.listen(port, console.log(`This is server is poppin off`))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
+  );
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`This is server is poppin off`));
