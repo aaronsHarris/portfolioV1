1e8e0bcd32f84a47e7ec5f7431dc0ab3a708c9c7
Aaron Harris <aaronsharris@gmail.com>
2021-11-29 18:46:42 -0600

    added contactMe page, basic functions for page, images for page

--- client/src/PortfolioContainer/ContactMe/ContactMe.jsx
+++ client/src/PortfolioContainer/ContactMe/ContactMe.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import Typical from "react-typical";
 import imgBack from "../../assets/ContactMe/mailz.jpeg";
 import load1 from "../../assets/ContactMe/load2.gif";
 import ScreenHeading from "../../utils/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utils/ScrollService";
 import Animations from "../../utils/Animations";
-
 export default function ContactMe(props) {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [banner, setBanner] = useState("")
+    const [bool, setBool] = useState(false)
+
+
+
+
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
   };
   const fadeInSubscription =
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
-
+      ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+    
+    
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handleMessage = (e) => {
+        setMessage(e.target.value)
+    }
+            
   return (
     <div className="main-container" id={props.id || ""}>
       <ScreenHeading title={"Contact Me"} subHeading={"Keep in Touch"} />
@@ -28,7 +48,38 @@ export default function ContactMe(props) {
           <a href="https://www.linkedin.com/in/aaron-harris-577867218/">
             <i className="fa fa-linkedin"></i>
           </a>
-        </div>
+              </div>
+              <div className="back-form">
+                  <div className="img-back">
+                      <h4>Send your Email here!</h4>
+                      <img src={imgBack} alt="email" />
+                  </div>
+                  <form action="">
+                      <p>{banner}</p>
+                      <label htmlFor="name">Name</label>
+                      <input type="text"
+                      onChange={handleName}
+                      value={name}
+                      />
+
+                      <label htmlFor="email">Email</label>
+                      <input type="email" onChange={handleEmail}
+                      value={email}/>
+
+                      <label htmlFor="message">Message</label>
+                      <textarea type="text" onChange={handleMessage}
+                      value={message}/>
+
+                      <div className="send-btn">
+                          <button type='submit'>
+                              send
+                              <i className='fa fa-paper-plane' />
+                              
+                          </button>
+                      </div>
+                  </form>
+
+              </div>
       </div>
     </div>
   );
